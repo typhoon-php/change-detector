3161f3dffb9567986d7f0ec1c5a20f210566938a
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-05-05 11:44:50 +0300

    Move DeterministicChangeDetector to src

---
+++ DeterministicChangeDetector.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Typhoon\ChangeDetector;
+
+/**
+ * @api
+ */
+final class DeterministicChangeDetector implements ChangeDetector
+{
+    /**
+     * @param non-empty-string $id
+     */
+    public function __construct(
+        private readonly string $id,
+        private readonly bool $changed,
+    ) {}
+
+    public function changed(): bool
+    {
+        return $this->changed;
+    }
+
+    public function deduplicate(): array
+    {
+        return [$this->id => $this];
+    }
+}
